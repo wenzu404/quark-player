28fac2cea073657197531b80e24e54f00361a6c0
Alexander Frick <Alex313031@gmail.com>
2023-06-21 05:41:41 -0500

    minor uprev

--- src/menu.js
+++ src/menu.js
@@ -473,6 +473,7 @@ module.exports = (store, services, mainWindow, app, defaultUserAgent) => {
         {
           label: 'Open Test Image',
           visible: process.env.QUARK_TEST === '1',
+          accelerator: 'CmdorCtrl+Alt+Shift+T',
           click() {
             const yiffWindow = new BrowserWindow({width: 700, height: 932, title: "Juno's Ass"});
             electronLog.info('Opening test image')
