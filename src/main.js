ffc9a70852255326d6c8e15d1a3716cd4b11d085
Alexander Frick <Alex313031@gmail.com>
2024-02-04 21:58:33 -0600

    mac active

--- src/main.js
+++ src/main.js
@@ -1120,6 +1120,9 @@ app.on('window-all-closed', () => {
 // On macOS it's common to re-create a window in the app when the
 // dock icon is clicked and there are no other windows open.
 app.on('activate', () => {
+  if (isMac) {
+    mainWindow.focus();
+  }
   //if (mainActivated == null) {
       electronLog.info('App Re-Activated [ Loading main.js ]');
   //  createWindow();
